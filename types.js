ec46cdca35cf71566f46c2b3b124f721eb431df8
thangln <thangln.hn@gmail.com>
2022-01-03 20:10:23 +0700

    Add files via upload

---
+++ types.js
@@ -0,0 +1,17 @@
+// 8 types data:
+/*
+1. number
+2. bigint
+3. string
+4. bool
+5. null
+6. undefined
+7. objects
+8. typeof
+*/
+
+let typeData = 'Thang';
+console.log(typeof typeData);
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${name}`);
---
+++ variables.js
@@ -0,0 +1,9 @@
+let admin;
+let name;
+
+name = "John";
+console.log("name == ",name);
+console.log();
+
+admin = name;
+console.log("admin == ",admin);
